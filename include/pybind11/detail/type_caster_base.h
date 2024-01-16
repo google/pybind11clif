cdb2b888a748bc8b56815716b4877c99c8dd553c
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-01-15 16:09:46 -0800

    Add `case return_value_policy::_clif_automatic` in type_caster_base.h

--- include/pybind11/detail/type_caster_base.h
+++ include/pybind11/detail/type_caster_base.h
@@ -580,6 +580,7 @@ public:
                 wrapper->owned = false;
                 break;
 
+            case return_value_policy::_clif_automatic:
             case return_value_policy::copy:
                 if (copy_constructor) {
                     valueptr = copy_constructor(src);
