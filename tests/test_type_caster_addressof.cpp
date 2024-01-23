82c3482a76c5b0a0fd57238488f1efb6761cfe22
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-01-22 21:19:38 -0800

    Also cover `CastConstRef` (to be verified).

--- tests/test_type_caster_addressof.cpp
+++ tests/test_type_caster_addressof.cpp
@@ -25,8 +25,10 @@ PYBIND11_SMART_HOLDER_TYPE_CASTERS(pybind11_tests::type_caster_addressof::Unusua
 namespace pybind11_tests {
 namespace type_caster_addressof {
 
-py::object PassMovable0(UnusualOpRef<0> &&mvbl) { return py::cast(std::move(mvbl)); }
-py::object PassMovable1(UnusualOpRef<1> &&mvbl) { return py::cast(std::move(mvbl)); }
+py::object CastConstRef0(const UnusualOpRef<0> &cref) { return py::cast(cref); }
+py::object CastConstRef1(const UnusualOpRef<1> &cref) { return py::cast(cref); }
+py::object CastMovable0(UnusualOpRef<0> &&mvbl) { return py::cast(std::move(mvbl)); }
+py::object CastMovable1(UnusualOpRef<1> &&mvbl) { return py::cast(std::move(mvbl)); }
 
 } // namespace type_caster_addressof
 } // namespace pybind11_tests
@@ -35,6 +37,8 @@ TEST_SUBMODULE(type_caster_addressof, m) {
     using namespace pybind11_tests::type_caster_addressof;
     py::class_<UnusualOpRef<0>>(m, "UnusualOpRef0");
     py::classh<UnusualOpRef<1>>(m, "UnusualOpRef1");
-    m.def("CallPassMovable0", []() { return PassMovable0(UnusualOpRef<0>()); });
-    m.def("CallPassMovable1", []() { return PassMovable1(UnusualOpRef<1>()); });
+    m.def("CallCastConstRef0", []() { return CastConstRef0(UnusualOpRef<0>()); });
+    m.def("CallCastConstRef1", []() { return CastConstRef1(UnusualOpRef<1>()); });
+    m.def("CallCastMovable0", []() { return CastMovable0(UnusualOpRef<0>()); });
+    m.def("CallCastMovable1", []() { return CastMovable1(UnusualOpRef<1>()); });
 }
