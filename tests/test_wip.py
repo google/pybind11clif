53e816004265d738617a3f92e16d7b6491b4f474
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-01-25 15:50:49 -0800

    Add `test_abc_meta_incompatibility()`, `test_abc_meta_compatibility()` to test_methods_and_attributes.py

--- tests/test_wip.py
+++ tests/test_wip.py
@@ -1,12 +1,5 @@
-import abc
-
 from pybind11_tests import wip as m
-
-
-class VanillaABC(m.Vanilla, metaclass=abc.ABCMeta):
-    pass
 
 
 def test_vanilla():
     assert m.Vanilla().__class__.__name__ == "Vanilla"
-    assert type(m.Vanilla).__name__ == "type"
