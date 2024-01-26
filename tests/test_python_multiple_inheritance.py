9c3799cabde3326e7ea7c1e82c154e772683e660
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-01-26 12:53:35 -0800

    Transfer additional tests from PyCLIF python_multiple_inheritance_test.py

--- tests/test_python_multiple_inheritance.py
+++ tests/test_python_multiple_inheritance.py
@@ -1,5 +1,7 @@
 # Adapted from:
-# https://github.com/google/clif/blob/5718e4d0807fd3b6a8187dde140069120b81ecef/clif/testing/python/python_multiple_inheritance_test.py
+# https://github.com/google/clif/blob/7d388e1de7db5beeb3d7429c18a2776d8188f44f/clif/testing/python/python_multiple_inheritance_test.py
+
+import pytest
 
 from pybind11_tests import python_multiple_inheritance as m
 
@@ -10,6 +12,21 @@ class PC(m.CppBase):
 
 class PPCC(PC, m.CppDrvd):
     pass
+
+
+class PCExplicitInitWithSuper(m.CppBase):
+    def __init__(self, value):
+        super().__init__(value + 1)
+
+
+class PCExplicitInitMissingSuper(m.CppBase):
+    def __init__(self, value):
+        del value
+
+
+class PCExplicitInitMissingSuper2(m.CppBase):
+    def __init__(self, value):
+        del value
 
 
 def test_PC():
@@ -33,3 +50,20 @@ def test_PPCC():
     d.reset_base_value_from_drvd(30)
     assert d.get_base_value() == 30
     assert d.get_base_value_from_drvd() == 30
+
+
+def testPCExplicitInitWithSuper():
+    d = PCExplicitInitWithSuper(14)
+    assert d.get_base_value() == 15
+
+
+@pytest.mark.parametrize(
+    ("derived_type"), [PCExplicitInitMissingSuper, PCExplicitInitMissingSuper2]
+)
+def testPCExplicitInitMissingSuper(derived_type):
+    derived_type = PCExplicitInitMissingSuper
+    with pytest.raises(TypeError) as excinfo:
+        derived_type(0)
+    assert str(excinfo.value).endswith(
+        "python_multiple_inheritance.CppBase.__init__() must be called when overriding __init__"
+    )
