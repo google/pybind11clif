94c268c0959fc263a0b6b328b01c2eed2da96078
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-04-01 14:52:49 -0700

    Add back additional tests.

--- tests/test_python_multiple_inheritance.cpp
+++ tests/test_python_multiple_inheritance.cpp
@@ -28,8 +28,28 @@ private:
     int drvd_value;
 };
 
+template <int SerNo>
+struct CppDrvd2 : CppBase<SerNo> {
+    explicit CppDrvd2(int value) : CppBase<SerNo>(value), drvd2_value(value * 5) {}
+    int get_drvd2_value() const { return drvd2_value; }
+    void reset_drvd2_value(int new_value) { drvd2_value = new_value; }
+
+    int get_base_value_from_drvd2() const { return CppBase<SerNo>::get_base_value(); }
+    void reset_base_value_from_drvd2(int new_value) {
+        CppBase<SerNo>::reset_base_value(new_value);
+    }
+
+private:
+    int drvd2_value;
+};
+
 template <int SerNo, typename... Extra>
-void wrap_classes(py::module_ &m, const char *name_base, const char *name_drvd, Extra... extra) {
+void wrap_classes(py::module_ &m,
+                  const char *name_base,
+                  const char *name_drvd,
+                  const char *name_drvd2,
+                  const char *pass_base,
+                  Extra... extra) {
     py::class_<CppBase<SerNo>>(m, name_base, std::forward<Extra>(extra)...)
         .def(py::init<int>())
         .def("get_base_value", &CppBase<SerNo>::get_base_value)
@@ -41,14 +61,28 @@ void wrap_classes(py::module_ &m, const char *name_base, const char *name_drvd, 
         .def("reset_drvd_value", &CppDrvd<SerNo>::reset_drvd_value)
         .def("get_base_value_from_drvd", &CppDrvd<SerNo>::get_base_value_from_drvd)
         .def("reset_base_value_from_drvd", &CppDrvd<SerNo>::reset_base_value_from_drvd);
+
+    py::class_<CppDrvd2<SerNo>, CppBase<SerNo>>(m, name_drvd2, std::forward<Extra>(extra)...)
+        .def(py::init<int>())
+        .def("get_drvd2_value", &CppDrvd2<SerNo>::get_drvd2_value)
+        .def("reset_drvd2_value", &CppDrvd2<SerNo>::reset_drvd2_value)
+        .def("get_base_value_from_drvd2", &CppDrvd2<SerNo>::get_base_value_from_drvd2)
+        .def("reset_base_value_from_drvd2", &CppDrvd2<SerNo>::reset_base_value_from_drvd2);
+
+    m.def(pass_base, [](const CppBase<SerNo> *) {});
 }
 
 } // namespace test_python_multiple_inheritance
 
 TEST_SUBMODULE(python_multiple_inheritance, m) {
     using namespace test_python_multiple_inheritance;
-    wrap_classes<0>(m, "CppBase0", "CppDrvd0");
-    wrap_classes<1>(m, "CppBase1", "CppDrvd1", py::metaclass((PyObject *) &PyType_Type));
+    wrap_classes<0>(m, "CppBase0", "CppDrvd0", "CppDrvd20", "pass_CppBase0");
+    wrap_classes<1>(m,
+                    "CppBase1",
+                    "CppDrvd1",
+                    "CppDrvd21",
+                    "pass_CppBase1",
+                    py::metaclass((PyObject *) &PyType_Type));
 
     m.attr("if_defined_PYBIND11_INIT_SAFETY_CHECKS_VIA_DEFAULT_PYBIND11_METACLASS") =
 #if defined(PYBIND11_INIT_SAFETY_CHECKS_VIA_DEFAULT_PYBIND11_METACLASS)
--- tests/test_python_multiple_inheritance.py
+++ tests/test_python_multiple_inheritance.py
@@ -33,6 +33,28 @@ class PCExplicitInitMissingSuperB0(m.CppBase0):
         del value
 
 
+class PPPCCC0(PPCC0, m.CppDrvd20):
+    pass
+
+
+class PC10(m.CppDrvd0):
+    pass
+
+
+class PC20(m.CppDrvd20):
+    pass
+
+
+class PCD0(PC10, PC20):
+    pass
+
+
+class PCDI0(PC10, PC20):
+    def __init__(self):
+        PC10.__init__(self, 11)
+        PC20.__init__(self, 12)
+
+
 #
 # Using py::metaclass((PyObject *) &PyType_Type) (used with py::class_<> for CppBase1, CppDrvd1):
 # COPY-PASTE block from above, replace 0 with 1:
@@ -60,6 +82,28 @@ class PCExplicitInitMissingSuper1(m.CppBase1):
 class PCExplicitInitMissingSuperB1(m.CppBase1):
     def __init__(self, value):
         del value
+
+
+class PPPCCC1(PPCC1, m.CppDrvd21):
+    pass
+
+
+class PC11(m.CppDrvd1):
+    pass
+
+
+class PC21(m.CppDrvd21):
+    pass
+
+
+class PCD1(PC11, PC21):
+    pass
+
+
+class PCDI1(PC11, PC21):
+    def __init__(self):
+        PC11.__init__(self, 11)
+        PC21.__init__(self, 12)
 
 
 @pytest.mark.parametrize(("pc_type"), [PC0, PC1])
@@ -136,3 +180,51 @@ def test_derived_tp_init_registry_weakref_based_cleanup():
     for _ in range(100):
         assert nested_function(0) == (10, 11)
         assert nested_function(3) == (13, 14)
+
+
+def NOtest_PPPCCC0():
+    # terminate called after throwing an instance of 'pybind11::error_already_set'
+    # what():  TypeError: bases include diverging derived types:
+    #     base=pybind11_tests.python_multiple_inheritance.CppBase0,
+    #     derived1=pybind11_tests.python_multiple_inheritance.CppDrvd0,
+    #     derived2=pybind11_tests.python_multiple_inheritance.CppDrvd20
+    PPPCCC0(11)
+
+
+def NOtest_PPPCCC1():
+    # terminate called after throwing an instance of 'pybind11::error_already_set'
+    # what():  TypeError: bases include diverging derived types:
+    #     base=pybind11_tests.python_multiple_inheritance.CppBase1,
+    #     derived1=pybind11_tests.python_multiple_inheritance.CppDrvd1,
+    #     derived2=pybind11_tests.python_multiple_inheritance.CppDrvd21
+    PPPCCC1(11)
+
+
+@pytest.mark.parametrize(
+    ("pcd_type", "cppdrvd2"), [(PCD0, "CppDrvd20"), (PCD1, "CppDrvd21")]
+)
+def test_PCD(pcd_type, cppdrvd2):
+    # This escapes all_type_info_check_for_divergence() because CppBase does not appear in bases.
+    with pytest.raises(
+        TypeError,
+        match=cppdrvd2 + r"\.__init__\(\) must be called when overriding __init__$",
+    ):
+        pcd_type(11)
+
+
+def test_PCD1():
+    # This escapes all_type_info_check_for_divergence() because CppBase0 does not appear in bases.
+    with pytest.raises(
+        TypeError,
+        match=r"CppDrvd21\.__init__\(\) must be called when overriding __init__$",
+    ):
+        PCD1(11)
+
+
+@pytest.mark.parametrize(
+    ("pcdi_type", "pass_fn"), [(PCDI0, m.pass_CppBase0), (PCDI1, m.pass_CppBase1)]
+)
+def test_PCDI(pcdi_type, pass_fn):
+    obj = pcdi_type()
+    with pytest.raises(TypeError, match="^bases include diverging derived types: "):
+        pass_fn(obj)
