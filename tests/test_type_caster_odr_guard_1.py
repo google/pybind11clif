98af64129797dd4927761b42ef4993dc62926a1f
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-07-20 18:50:28 -0700

    Fix ruff error (not sure why this passed when testing locally).

--- tests/test_type_caster_odr_guard_1.py
+++ tests/test_type_caster_odr_guard_1.py
@@ -45,7 +45,6 @@ def test_type_caster_odr_violation_detected_counter():
     num_violations = m.type_caster_odr_violation_detected_count()
     if num_violations is None:
         pytest.skip("type_caster_odr_violation_detected_count() is None")
-    if num_violations == 0:
-        if m.if_defined__NO_INLINE__:
-            pytest.skip(_count_0_message("__NO_INLINE__"))
+    if num_violations == 0 and m.if_defined__NO_INLINE__:
+        pytest.skip(_count_0_message("__NO_INLINE__"))
     assert num_violations == 1
